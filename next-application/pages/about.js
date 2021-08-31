dab9a34f5a3f5c8aab76e1495b2cc0912d61bc5d
satish-313 <pradhansatish53@gmail.com>
2021-08-31 09:46:49 +0700

    next nextapp

---
+++ next-application/pages/about.js
@@ -0,0 +1,15 @@
+import Head from "next/head"
+
+const About = () => {
+    return (
+        <div>
+            <Head>
+                <title>about page</title>
+                <meta  name="dev" content="web development, programming,blog,cms"/>
+            </Head>
+            <h1>about page</h1>
+        </div>
+    )
+}
+
+export default About;
